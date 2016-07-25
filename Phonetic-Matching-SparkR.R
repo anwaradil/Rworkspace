7eaedddd8b4ba9b3b7fd534e13870177005c9104
anwaradil <anwar.adil@live.com>
2016-07-25 12:30:07 -0700

    Updated to make public

--- Phonetic-Matching-SparkR.R
+++ Phonetic-Matching-SparkR.R
@@ -19,13 +19,11 @@ dataset1<-read.csv(file.choose(), header=FALSE,colClasses="character",as.is=TRUE
 dataset2<-read.csv(file.choose(), header=FALSE,colClasses="character",as.is =TRUE,na.strings=NULL,sep="\t")
 dataset2
 
+#dataset2
+
 names(dataset1)<-c("FNAME","LNAME")
 names(dataset2)<-c("FNAME","LNAME")
 
-#dataset2
-
-#names(datasetURL)<-c("tin","firstName","middleName","lastName","provider_id","providerfullname","addressline1","addressline2","city","state","zipcode")
-#names(datasetNDB)<-c("prov_id","tax_id","lastname","firstname","midname","adr_id","addressline1","city","state","zipcode","canc_dt")
 
 
 df1<-data.frame(dataset1$FNAME,dataset1$LNAME,fullname1=paste(dataset1$FNAME,dataset1$LNAME))
@@ -36,7 +34,7 @@ df1
 df2
 
 
-
+# use trim function if you need to standardize the data
 trim<- function (x) {
   x<- tolower(x)
   x<- gsub("^\\s+\\s+$","", x)
@@ -53,7 +51,7 @@ trim<- function (x) {
 
 
 
-#df1$pfullName1<-trim((df1$fullname1))
+#df1$pfullName1<-trim((df1$fullname1)) - Call Trim function from here . This is commented out for now.
 df1$fullname1
 df1$NamePhonetic<-phonetic(df1$fullname1,method=c("soundex"),useBytes=FALSE)
 
@@ -62,23 +60,20 @@ df1$NamePhonetic<-phonetic(df1$fullname1,method=c("soundex"),useBytes=FALSE)
 df2$fullname2
 df2$NamePhonetic<-phonetic(df2$fullname2,method=c("soundex"),useBytes=FALSE)
 namematchdf<-merge(df1,df2,by="NamePhonetic",all=TRUE)
-#namematchdf<-c(namematchdf)
-#namematchdf<-as.character(namematchdf)
+
 #Sys.setlocale('LC_ALL','C')
 namematchdf <- data.frame(lapply(namematchdf, as.character), stringsAsFactors=FALSE)
 #is.character(namematchdf$dataset1.FNAME)
 namematchdf$namesimscore<-levenshteinSim(namematchdf$fullname1,namematchdf$fullname2)
-write.table(namematchdf,file="test.txt",col.names=TRUE,quote=FALSE,sep="\t")
+#write.table(namematchdf,file="test.txt",col.names=TRUE,quote=FALSE,sep="\t")
 namematchdf<- createDataFrame(sqlContext, namematchdf)
 
 #Query
 perfectScore <- filter(namematchdf, namematchdf$namesimscore == 1)
 
-
+head(perfectScore)
 
 head(arrange(namematchdf, desc(namematchdf$namesimscore)))
-
-head(perfectScore)
 
 
 sparkR.stop()
